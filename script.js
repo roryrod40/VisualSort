79308b1cc3347eea8174bfa777779b8a882516cd
roryrod40 <rodriguez.rory40@gmail.com>
2023-05-12 23:04:54 -0400

    Updated Bubble Sort

--- script.js
+++ script.js
@@ -9,14 +9,19 @@ const maxElement = 100;
 let timePerOperation = 33;
 let arrIsGen = false;
 let canClick = true;
+let canClickSort = false;
 const headTitles = document.querySelectorAll(".head_title");
+const sortTitles = document.querySelectorAll(".sort");
 const genArrBtn = document.querySelector(".gen_arr");
 const inputMsg = document.querySelector(".input_msg");
 const arrContianer = document.querySelector(".arr_container");
 let arrElements = document.querySelectorAll(".bar");
 const bubSortBtn = document.querySelector(".bub_sort");
+const merSortBtn = document.querySelector(".mer_sort");
 const speedSlider = document.querySelector(".speed");
 const sizeSlider = document.querySelector(".size");
+const currentColor1 = "#4f759b";
+const completedColor = "#436436";
 
 /*
  * Get speed and size of arrays
@@ -48,7 +53,7 @@ function printArr() {
     const element = document.createElement("div");
     element.classList.add("bar");
     element.setAttribute("style", `--bar-value:${num}%;`);
-    element.setAttribute("data", num);
+    element.data = num;
     element.style.width = 80 / elementNum + "%";
     element.appendChild(data);
     arrContianer.appendChild(element);
@@ -58,12 +63,11 @@ function printArr() {
 /*
  * Reset Color Array Element Function
  */
-function resetColor() {
+function resetColor(i) {
   arrElements = document.querySelectorAll(".bar");
-  arrElements.forEach((element) => {
-    element.style.backgroundColor = "";
-    element.firstChild.style.color = "";
-  });
+  const element = arrElements[i];
+  element.style.backgroundColor = "";
+  element.firstChild.style.color = "";
 }
 
 /*
@@ -89,7 +93,24 @@ function toggleCanClick() {
   } else {
     canClick = true;
     headTitles.forEach((title) => {
-      title.style.cursor = "";
+      title.style.cursor = "pointer";
+    });
+  }
+}
+
+/*
+ * Toggle Can Click Sort Function
+ */
+function toggleCanClickSort() {
+  if (canClickSort) {
+    canClickSort = false;
+    sortTitles.forEach((title) => {
+      title.style.cursor = "default";
+    });
+  } else {
+    canClickSort = true;
+    sortTitles.forEach((title) => {
+      title.style.cursor = "pointer";
     });
   }
 }
@@ -98,20 +119,18 @@ function toggleCanClick() {
  * Generate Array Button
  */
 function generateArray() {
+  toggleCanClickSort();
   arrIsGen = true;
-  if (canClick) {
-    arr = Array(elementNum);
-    // Create array with random elements
-    for (let i = 0; i < arr.length; i++) {
-      arr[i] = Math.trunc(Math.random() * maxElement);
-    }
-    console.log(arr);
-
-    // Remove input message from HTML if present
-    if (inputMsg) inputMsg.textContent = "Random array generated!";
-
-    printArr();
+  if (!canClick) return;
+  arr = Array(elementNum);
+  // Create array with random elements
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = Math.trunc(Math.random() * maxElement);
   }
+  // Print message and array
+  inputMsg.textContent =
+    "Random array generated! Choose a sorting algorithm :)";
+  printArr();
 }
 genArrBtn.addEventListener("click", generateArray);
 
@@ -119,35 +138,55 @@ genArrBtn.addEventListener("click", generateArray);
  * Generate Bubble Sort Button
  */
 function bubbleSort() {
+  // Return if sort cannot be done
+  if (!arrIsGen || !canClick || !canClickSort) return;
+  // Print runtime
+  inputMsg.textContent = "Bubble sort runtime is O(n^2)!";
+  toggleCanClick();
   // Preform BubbleSort
-  if (canClick && arrIsGen) {
-    // Print runtime
-    inputMsg.textContent = "Bubble sort runtime is O(n^2)!";
-    toggleCanClick();
-    let len = arr.length;
-    let num = 1;
-    for (let i = 0; i < len - 1; i++) {
-      for (let j = 0; j < len - i - 1; j++) {
-        // Preform operations after a certian amount of time
-        setTimeout(() => {
-          // Color two elements being looked at
-          printArr();
-          colorArr(j, "#a44a3f");
-          colorArr(j + 1, "#a44a3f");
-          if (arr[j] > arr[j + 1]) {
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
-          }
-        }, num * timePerOperation);
-        num++;
-      }
+  let len = arr.length;
+  let num = 1;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      // Preform operations after a certian amount of time
+      setTimeout(() => {
+        arrElements = document.querySelectorAll(".bar");
+        if (arr[j] > arr[j + 1]) {
+          // Update array elements
+          let temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+          // Update elements in HTML
+          arrElements[j].setAttribute("style", `--bar-value:${arr[j]}%;`);
+          arrElements[j].setAttribute("data", arr[j]);
+          arrElements[j].firstChild.textContent = arr[j];
+          arrElements[j].style.width = 80 / arr.length + "%";
+          arrElements[j + 1].setAttribute(
+            "style",
+            `--bar-value:${arr[j + 1]}%;`
+          );
+          arrElements[j + 1].setAttribute("data", arr[j + 1]);
+          arrElements[j + 1].firstChild.textContent = arr[j + 1];
+          arrElements[j + 1].style.width = 80 / arr.length + "%";
+        }
+        if (j - 1 >= 0) resetColor(j - 1);
+        colorArr(j, currentColor1);
+        colorArr(j + 1, currentColor1);
+      }, num * timePerOperation);
+      num++;
     }
-    // Reset color to default color and allow other buttons to be clicked
     setTimeout(() => {
-      resetColor();
-      toggleCanClick();
+      colorArr(arr.length - 1 - i, completedColor);
+      resetColor(arr.length - 2 - i);
     }, num * timePerOperation);
   }
+  // Reset color to default color and allow other buttons to be clicked
+  setTimeout(() => {
+    colorArr(0, completedColor);
+    toggleCanClick();
+    toggleCanClickSort();
+    inputMsg.textContent =
+      "Congrats your array is now sorted! Click Generate Array to try again!";
+  }, num * timePerOperation);
 }
 bubSortBtn.addEventListener("click", bubbleSort);
